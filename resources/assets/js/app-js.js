c35c678711498ef12ae0f972e48650cc99abd5be
Roman Kinyakin <1@grep.su>
2015-11-19 15:38:58 +0600

    Fix app-js to work without requirejs

--- resources/assets/js/app-js.js
+++ resources/assets/js/app-js.js
@@ -1,7 +1,16 @@
-define(['jquery', 'bootstrap', 'slick'], function($) {
+(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(function($) {
 
     if (typeof $ === 'undefined') {
         console.log('Не удалось загрузить jQuery');
     }
 
-});
+}));
